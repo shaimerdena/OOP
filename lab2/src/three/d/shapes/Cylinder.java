f731d6f0af31c6584bf8e210506a31b51bd10dc8
Ayau <shaymerdena@gmail.com>
2026-03-05 21:12:19 +0500

    update

---
+++ lab2/src/three/d/shapes/Cylinder.java
@@ -0,0 +1,18 @@
+package three.d.shapes;
+
+public class Cylinder extends Shape3D {
+	
+	
+	@Override
+	public double volume() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double surfaceArea() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
