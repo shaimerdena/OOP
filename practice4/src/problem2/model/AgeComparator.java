2d40fa44dd70ee18a0c2ccab8ad446e4de774fbd
Ayau <shaymerdena@gmail.com>
2026-03-27 21:32:19 +0500

    feat: added comparator and comparable

---
+++ practice4/src/problem2/model/AgeComparator.java
@@ -0,0 +1,9 @@
+package problem2.model;
+
+import java.util.Comparator;
+
+public class AgeComparator implements Comparator<Person>{
+	public int compare(Person p1, Person p2) {
+		return Integer.compare(p1.age, p2.age);
+	}
+}
--- practice4/src/problem2/model/Person.java
+++ practice4/src/problem2/model/Person.java
@@ -1,5 +1,10 @@
 package problem2.model;
 
-public abstract class Person {
-
+public abstract class Person implements Comparable<Person> {
+	public String name;
+	public int age;
+	
+	public int compareTo(Person other) {
+		return this.name.compareTo(other.name);
+	}
 }
